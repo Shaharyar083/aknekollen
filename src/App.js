1d2a4179bd892b60885640eb635b0daaa71c519a
abdulqayyum686 <qayyuma686@gmail.com>
2022-06-04 22:57:55 +0500

    add second section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // components
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/Aknekollen/HeroSection/HeroSection";
+import OpeningHours from "./components/OpeningHours/OpeningHours";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <OpeningHours />
     </>
   );
 }
