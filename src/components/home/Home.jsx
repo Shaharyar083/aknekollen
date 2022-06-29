12d06c2406bc2853118774d28267c697a1138d21
shaharyar083 <shaharyarilyas7@gmail.com>
2022-06-30 02:24:31 +0500

    google analytics

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import react from "react";
 import Navbar from "../Navbar/Navbar";
 import HeroSection from "../Aknekollen/HeroSection/HeroSection";
 import Modal from "../Modal/Modal";
@@ -6,27 +6,36 @@ import OpeningHours from "../OpeningHours/OpeningHours";
 import AboutSection from "../AboutSection/AboutSection";
 import Footer from "../Footer/index";
 import FactsSection from "../FactsSection/FactsSection";
-const Home = () => {
-    const [modal, setModal] = react.useState(false);
-    return (
-        <>
-            {
-                modal ? (
-                    <Modal close={() => setModal(false)} />
-                ) : (
-                    <>
-                        <Navbar />
-                        <HeroSection openModal={() => setModal(true)} />
-                        <OpeningHours />
-                        <AboutSection />
-                        <FactsSection />
-                        {/* <Fakta /> */}
-                        <Footer />
-                    </>
-                )
-            }
-        </>
-    )
-}
 
-export default Home
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "UA-220438183-5";
+ReactGA.initialize(TRACKING_ID);
+
+const Home = () => {
+  const [modal, setModal] = react.useState(false);
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+    <>
+      {modal ? (
+        <Modal close={() => setModal(false)} />
+      ) : (
+        <>
+          <Navbar />
+          <HeroSection openModal={() => setModal(true)} />
+          <OpeningHours />
+          <AboutSection />
+          <FactsSection />
+          {/* <Fakta /> */}
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
